90aa7fa00a84326fbffaeefbbf66fe39f6573c5d
gadrinf <gadrincraftpl@gmail.com>
2017-02-08 15:45:31 +0000

    Add files via upload

--- src/com/flowrstuff/graphics/Dimension.java
+++ src/com/flowrstuff/graphics/Dimension.java
@@ -1,74 +1,55 @@
 package com.flowrstuff.graphics;
 
-import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
 
-import javax.swing.JPanel;
 
-@SuppressWarnings("serial")
-public class Dimension extends JPanel implements Runnable{
+public class Dimension {
 	private int width, height, gridSizeX, gridSizeY;
 	private Color c1, c2;
 	private Graphics g;
-	private BufferStrategy bs;
-	private Canvas canvas;
 	
 	
-	public Dimension(int width, int height, int gridSizeX, int gridSizeY, Color c1, Color c2){
+	public Dimension(int width, int height, int gridSizeX, int gridSizeY, Color c1, Color c2, Graphics g){
 		this.width = width;
 		this.gridSizeX = gridSizeX;
 		this.gridSizeY = gridSizeY;
 		this.height = height;
+		this.g = g;
 		
 		//The two colours of the Dimension grid
 		this.c1 = c1;
 		this.c2 = c2;
 		
+		
 	}
 	
-	public void DrawDimension(){
-		setSize(width, height);
-		canvas = new Canvas();
-		this.add(canvas);
-		
-		canvas.createBufferStrategy(2);
-		
-		while(true){
-			Update();
-			Render();
-		}
-	}
+
 	public void Update(){
 
 	}
 	
 	public void Render(){
-		if(bs == null){
-			bs = canvas.getBufferStrategy();
-			g = bs.getDrawGraphics();
-		}
-		
-		
+
 		
 		DrawGrid();
-		
-		
+
 	}
 
-	@Override
-	public void run() {
-		DrawDimension();
-	}
 	
 	private void DrawGrid(){
-		int gridDimensionX = width * gridSizeX;
-		int gridDimensionY = height * gridSizeY;
+		int gridDimensionX = width / gridSizeX;
+		int gridDimensionY = height / gridSizeY;
 		for(int x = 0; x < gridSizeX; x++){
 			for(int y = 0; y < gridSizeY; y++){
-				g.setColor(c1);
-				g.fillRect(x * , y, x, y);
+				if(x % 2 == 0 && y % 2 == 0){
+					g.setColor(c1);
+				}else if(x % 2 == 1 && y % 2 == 1){
+					g.setColor(c1);
+				}else{
+					g.setColor(c2);
+				}
+				g.fillRect(x * gridDimensionX, y * gridDimensionY, gridDimensionX, gridDimensionY);
 			}
 		}
 	}
--- src/com/flowrstuff/main/Board.java
+++ src/com/flowrstuff/main/Board.java
@@ -1,16 +1,23 @@
 package com.flowrstuff.main;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.image.BufferStrategy;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import com.flowrstuff.graphics.Dimension;
 import com.flowrstuff.settings.Display;
 
 public class Board implements Runnable{
 	public Graphics g;
 	public BufferStrategy bs;
+	public Canvas c;
 	public Board(){
 		
 	}
@@ -18,12 +25,13 @@ public class Board implements Runnable{
 	//Setting up the main display of the ingame board window
 	public void BoardSetup(){
 		CreateWindow();
-		CreateGraphics();
+		Initialise();
 		
 	}
 	
+	JFrame boardWindow;
 	private void CreateWindow(){
-		JFrame boardWindow = new JFrame("DragonChess");
+		boardWindow = new JFrame("DragonChess");
 		boardWindow.setSize(Display.width, Display.height);
 		
 		
@@ -49,12 +57,33 @@ public class Board implements Runnable{
 		
 	}
 	
-	private void CreateGraphics(){
+	Thread board1Thread;
+	Dimension board1;
+	private void Initialise(){
+		//Creating a canvas that a single Dimension will be drawn on
+		JPanel panel = new JPanel();
+		panel.setSize(800, 600);
+		c = new Canvas();
+		panel.add(c);
+		//boardWindow.add(panel);
 		
 		
-		
+		boardWindow.setLayout(new GridBagLayout());
+		GridBagConstraints co = new GridBagConstraints();
+		boardWindow.add(new JButton("hello"));
+		boardWindow.add(new JButton("hello"));
+		boardWindow.add(new JButton("hello"));
+		boardWindow.add(new JButton("hello"));
+		boardWindow.add(panel);
+		//A buffer strategy of 2 frames should be more than enough for a simple chess game
+		c.createBufferStrategy(2);
+		bs = c.getBufferStrategy();
+		g = bs.getDrawGraphics();
+		board1 = new Dimension(480, 360, 12, 8, Color.blue, Color.gray, g);
+
 		while(true){
-			
+			Update();
+			Render();
 		}
 	}
 	
@@ -62,7 +91,10 @@ public class Board implements Runnable{
 		
 	}
 	private void Render(){
-		
+
+		board1.Render();
+		bs.show();
+		g.dispose();
 	}
 	
 	@Override
